c5c1dbe29310661657027b9cbaf7593163cd2338
“tonykochev” <tonykochev@gmail.com>
2026-01-01 14:23:07 -0600

    changed navbar icon to calender icon, made it primary color

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router"
-import { PlusIcon } from "lucide-react"
+import { CalendarDays } from "lucide-react"
 
 const NavBar = () => {
   return <header className='bg-base-300 border-b border-base-content/10'>
@@ -7,9 +7,8 @@ const NavBar = () => {
         <div className='flex items-center justify-between'>
             <h1 className='text-3xl, font-bold text-primary front-mono tracking-tight'>OnlineRunningCoach</h1>
             <div className="flex items-center gap-4">
-                <Link to={"/create"} className="btn btn-primary">
-                <PlusIcon className="size-5"/>
-                <span>New Note</span>
+                <Link to={"/create"} className="btn btn-ghost">
+                <CalendarDays className="size-7 text-primary"/>
                 </Link>
             </div>
         </div>
