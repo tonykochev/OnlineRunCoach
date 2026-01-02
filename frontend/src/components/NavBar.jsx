91169f50c98373419c3d6b341a3fb544bf82d5dd
“tonykochev” <tonykochev@gmail.com>
2026-01-01 21:33:25 -0600

    updated navbar, added icons for profile, notification and orginized the navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,18 +1,54 @@
 import { Link } from "react-router"
-import { CalendarDays } from "lucide-react"
+import { CalendarDays, Bell, User } from "lucide-react"
 
 const NavBar = () => {
-  return <header className='bg-base-300 border-b border-base-content/10'>
+  return (
+  <header className='bg-base-300 border-b border-base-content/10'>
     <div className='mx-auto max-w-6xl p-4'>
-        <div className='flex items-center justify-between'>
+        <div className='relative flex items-center justify-between'>
+
+            
+            <div className="flex items-center gap-4"> 
             <h1 className='text-3xl, font-bold text-primary front-mono tracking-tight'>OnlineRunningCoach</h1>
-            <div className="flex items-center gap-4">
-                <Link to={"/create"} className="btn btn-ghost">
+            
+
+
+            {/* Profile */}
+            <button className="btn btn-ghost btn-cirlce">
+                <User className="size-5 text-primary"/>
+            </button>
+
+            {/* Notifications */}
+            <button className="btn btn-ghost btn-circle">
+                <Bell className="size-5 text-primary" />
+            </button>
+            </div>
+
+        {/* CENTER */}
+        <div className="absolute left-1/2 -translate-x-1/2">
+            <div className="dropdown dropdown-center">
+                <div tabIndex={0} role="button" className="btn btn-ghost rounded-field m-1">Dropdown</div>
+                <ul tabIndex="-1" className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm left-1/2 -translate-x-1/2">
+                    <li><a>Week 1</a></li>
+                    <li><a>Week 2</a></li>
+                </ul>
+            </div>
+        </div>
+
+        {/* CENTER */}
+        <div className="ml-auto">
+            <Link to={"/create"} className="btn btn-ghost">
                 <CalendarDays className="size-7 text-primary"/>
-                </Link>
+            </Link>
+        </div>
+
+
+            <div className="flex items-center gap-4">
+                
             </div>
         </div>
     </div>
   </header>
+  )
 }
 export default NavBar
